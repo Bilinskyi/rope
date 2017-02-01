54eab6dcf07f766c077e36545112cd3c0aa05b07
Taras Bilinskyi <tarasbilinskyi@Mac-mini-Taras.local>
2017-02-01 19:05:48 +0200

    v0

---
+++ app/js/main.js
@@ -0,0 +1,60 @@
+$(window).on('load', function() {
+
+	$('.slick-prev, .slick-next').prepend('<div class="semi-circle"></div>');
+
+});
+
+$(document).ready(function(){
+
+
+if($('.slider-1').length) {
+	$('.slider-1').slick({
+
+		dots: true,
+		infinite: true,
+		speed: 500,
+		cssEase: 'ease'
+		// fade: true,
+
+	});
+}
+
+
+
+
+ //    $("a.modal-form").fancybox({
+ //       'hideOnContentClick': true,
+ //          minWidth : 230,
+ //          padding : 0,
+ //          closeBtn : true
+ 
+ // });
+
+// $('header a[href^="#"]').on('click', function(event) {
+
+//     var target = $( $(this).attr('href') );
+
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: target.offset().top -85
+//         }, 600);
+//     }
+
+// });
+
+
+
+//  jQuery(function($){ 
+//    $(".phone_mask_1").mask("+7(999) 999-9999");
+//    $("#phone_mask_2").mask("+7(999) 999-9999");
+// });
+
+$('input,textarea').focus(function(){
+	$(this).data('placeholder',$(this).attr('placeholder'))
+	.attr('placeholder','');
+}).blur(function(){
+	$(this).attr('placeholder',$(this).data('placeholder'));
+});
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,157 @@
+'use strict';
+
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+rename = require('gulp-rename'),
+sftp = require('gulp-sftp'),
+concat       = require('gulp-concat'),
+jsmin = require('gulp-jsmin'),
+browserSync = require('browser-sync').create(),
+imageop = require('gulp-image-optimization'),
+del         = require('del'), 
+autoprefixer = require('gulp-autoprefixer'),
+spritesmith  = require('gulp.spritesmith'),
+tiny = require('gulp-tinypng-nokey'),
+gcmq = require('gulp-group-css-media-queries'),
+cleanCSS = require('gulp-clean-css');
+
+
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+// group @media only screen and (max-width: 991px) 
+gulp.task('media', function () {
+    gulp.src('app/css/style.css')
+        .pipe(gcmq())
+        .pipe(gulp.dest('app/css'));
+});
+
+
+// optimize image
+gulp.task('tiny', function(cb) {
+  gulp.src('app/img/*')
+  .pipe(tiny())
+  .pipe(gulp.dest('image'));
+});
+
+
+
+// gulp.task('browserSync', function() {
+//     browserSync.init({
+//         proxy: "localhost:8888/appWP/"
+//     });
+// });
+
+
+gulp.task('images', function(cb) {
+	gulp.src(['app/img/**/*.png','app/img/**/*.jpg','app/img/**/*.gif','app/img/**/*.jpeg']).pipe(imageop({
+		optimizationLevel: 5,
+		progressive: true,
+		interlaced: true
+	})).pipe(gulp.dest('img_optimized')).on('end', cb).on('error', cb);
+});
+
+
+gulp.task('sass', function () {
+	return gulp.src('app/sass/**/*.scss')
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer(['last 15 versions'], { cascade: true }))
+	// .pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(gulp.dest('app/css/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+});
+
+// gulp.task('styles', function () {
+//   return gulp.src('app/css/*.css')
+//   .pipe(concatCss("style.css"))
+//   .pipe(cleanCSS({compatibility: 'ie8'}))
+//   .pipe(gulp.dest('dist/css'));
+// });
+
+gulp.task('scripts', function() {
+	return gulp.src([ 
+		'app/js/**/*.js',
+		'!app/js/**/main.js' 
+		])
+	.pipe(concat('libs.min.js')) 
+	.pipe(jsmin()) 
+	.pipe(gulp.dest('dist/js')); 
+});
+
+
+// gulp.task('clean', function() {
+// 	return del.sync('dist'); 
+// });
+
+// gulp.task('build', ['clean', 'images', 'sass', 'scripts' ], function() {
+
+// 	var buildCss = gulp.src([ 
+// 		'app/css/*.css'
+// 		])
+// 	.pipe(gulp.dest('dist/css'))
+
+// 	var buildFonts = gulp.src('app/fonts/**/*') 
+// 	.pipe(gulp.dest('dist/fonts'))
+
+	
+
+// 	var buildFancybox = gulp.src('app/fancybox/**/*') 
+// 	.pipe(gulp.dest('dist/fancybox'))
+
+// 	var buildJs = gulp.src([
+// 		'app/js/main.js'
+// 		]) 
+// 	.pipe(gulp.dest('dist/js'))
+
+// 	var buildHtml = gulp.src('app/*.html') 
+// 	.pipe(gulp.dest('dist'));
+
+// });
+
+
+gulp.task('sprite', function() {
+	var spriteData = 
+  gulp.src('app/spr/*.*') 
+  .pipe(spritesmith({
+   imgName: 'sprite.png',
+   imgPath: '../img/sprite.png',
+   padding: 5,
+   cssName: 'sprite.css',
+
+
+   cssOpts: {
+    cssSelector: function (item) {
+     if (item.name.indexOf('-hover') !== -1) {
+      return '.ico.' + item.name.replace('-hover', ':hover');
+    }
+    else {
+      return '.ico.' + item.name;
+    }
+  }
+}
+
+}));
+
+  spriteData.img.pipe(gulp.dest('app/img/')); 
+  spriteData.css.pipe(gulp.dest('app/css/')); 
+});
+
+
+
+
+gulp.task('default', ['sass', 'browserSync'], function () {
+	gulp.watch('app/sass/**/*.scss', ['sass']);
+// Reloads the browser whenever HTML or JS files change
+gulp.watch('app/*.html', browserSync.reload); 
+gulp.watch('app/*.css', browserSync.reload); 
+gulp.watch('app/*.php', browserSync.reload); 
+gulp.watch('app/js/**/*.js', browserSync.reload);
+});
